4674301220cd25efd4b4c1adceb480367ee7faaa
Rohit verma <28vrohit@gmail.com>
2022-09-09 19:28:06 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import { Box, Stack } from "@mui/material";
+import { Stack, Box } from "@mui/material";
 import Navbar from "./Components/Navbar";
 import Rightbar from "./Components/Rightbar";
 import Sidebar from "./Components/Sidebar";
 import Slides from "./Components/Slides";
+import Blog from "./Components/Blog";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Box>
-        <Navbar />
-        <Stack sx={{ m: 2, }} direction="row" spacing={1} justifyContent="space-between">
-          <Sidebar />
-          <Slides />
-          <Rightbar />
-        </Stack>
-      </Box>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={
+            <Box>
+              <Navbar />
+              <Stack sx={{ m: 2, }} direction="row" spacing={1} justifyContent="space-between">
+                <Sidebar />
+                <Slides />
+                <Rightbar />
+              </Stack>
+            </Box>
+          } />
+          <Route exact path="/blog" element={<Blog />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,8 +13,8 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import SatelliteAltIcon from '@mui/icons-material/SatelliteAlt';
 import RocketIcon from '@mui/icons-material/Rocket';
+import { Link } from 'react-router-dom';
 
-const pages = ['Products', 'Pricing', 'Blog'];
 
 const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -23,109 +23,137 @@ const Navbar = () => {
     setAnchorElNav(event.currentTarget);
   };
 
-  const handleCloseNavMenu = () => {
+  const handleCloseNavMenu = (e) => {
+    console.log(e)
     setAnchorElNav(null);
   };
 
-  return (
-      <AppBar position="sticky" sx={{ bgcolor: 'text.primary' }}>
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <SatelliteAltIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              sx={{
-                mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
-            >
-              RvVerse
-            </Typography>
 
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: 'block', md: 'none' },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            <SatelliteAltIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              sx={{
-                mr: 2,
-                display: { xs: 'flex', md: 'none' },
-                flexGrow: 1,
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
+  return (
+    <AppBar position="sticky" sx={{ bgcolor: 'text.primary' }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <SatelliteAltIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            RvVerse
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
             >
-              RvVerse
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  {page}
-                </Button>
-              ))}
-            </Box>
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="RvVerse" enterDelay={500} leaveDelay={3000}>
-                <IconButton sx={{ p: 0 }}>
-                  <Avatar sx={{ bgcolor: 'text.primary' }}>
-                    <RocketIcon fontSize='large' />
-                  </Avatar>
-                </IconButton>
-              </Tooltip>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' }, }}
+            >
+              <MenuItem>
+                <Link to="/blog" style={{ textDecoration: 'none' }}>
+                  <Typography sx={{ color: 'text.primary' }}>
+                    Blog
+                  </Typography>
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Link to="/blog" style={{ textDecoration: 'none' }}>
+                  <Typography sx={{ color: 'text.primary' }}>
+                    Blog_2
+                  </Typography>
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Link to="/blog" style={{ textDecoration: 'none' }}>
+                  <Typography sx={{ color: 'text.primary' }}>
+                    Blog_1
+                  </Typography>
+                </Link>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <SatelliteAltIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            RvVerse
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Link to="/blog" style={{ textDecoration: 'none' }}>
+              <Button
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                Blog
+              </Button>
+            </Link>
+            <Link to="/blog" style={{ textDecoration: 'none' }}>
+              <Button
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                Blog_1
+              </Button>
+            </Link>
+            <Link to="/blog" style={{ textDecoration: 'none' }}>
+              <Button
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                Blog_2
+              </Button>
+            </Link>
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="RvVerse" enterDelay={500} leaveDelay={3000}>
+              <IconButton sx={{ p: 0 }}>
+                <Avatar sx={{ bgcolor: 'text.primary' }}>
+                  <RocketIcon fontSize='large' />
+                </Avatar>
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 export default Navbar;
