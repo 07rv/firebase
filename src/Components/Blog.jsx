9318d7698a1ce4362b463d0185ab626e30731765
Rohit verma <28vrohit@gmail.com>
2022-09-11 06:14:46 +0530

    Update Blog.jsx

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,11 +1,9 @@
 import { Box } from '@mui/material'
 import React from 'react'
-import Navbar from './Navbar'
 
 const Blog = () => {
   return (
-    <Box>
-      <Navbar/>
+    <Box flex={4} p={2}>
     </Box>
   )
 }
