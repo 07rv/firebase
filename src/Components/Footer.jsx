596176d4ebac12307e9fd8e838e162a8e6192086
Rohit verma <28vrohit@gmail.com>
2022-09-14 20:43:38 +0530

    Create Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto voluptatem sit 
+        laborum tempore quisquam vero cupiditate est earum, 
+        nesciunt iste dicta molestias consequatur ab obcaecati quas illo esse harum non.</div>
+  )
+}
+
+export default Footer
