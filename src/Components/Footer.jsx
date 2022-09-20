b8f5e1790099e86f64f351b9e8d80b9eba9138f4
Rohit verma <28vrohit@gmail.com>
2022-09-20 08:47:05 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import {Box, Typography} from '@mui/material';
+import Link from '@mui/material/Link';
 
+function Copyright() {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
 const Footer = () => {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto voluptatem sit 
-        laborum tempore quisquam vero cupiditate est earum, 
-        nesciunt iste dicta molestias consequatur ab obcaecati quas illo esse harum non.</div>
+    <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </Box>
   )
 }
 
